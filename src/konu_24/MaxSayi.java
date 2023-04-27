b47962cbdf3be07280e2ba0d6caa5818bed9cd20
Mustafa <merguven75@mail.com>
2023-04-27 21:23:21 +0300

    day 27.04

---
+++ src/konu_24/MaxSayi.java
@@ -0,0 +1,35 @@
+package konu_24;
+
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class MaxSayi {
+        static Scanner scan=new Scanner(System.in);
+        static int max;
+        static int sayi;
+        public static void main(String[] args) {
+            // kullanicinin girdigi 5 sayidan en
+            //buyuk olan sayiyi yazdiriniz
+
+            int arr[]=new int[6];
+
+            for (int i = 1; i <arr.length; i++) {
+                System.out.println("lutfen " + i+ " inci sayiyi giriniz");
+                arr[i-1]=scan.nextInt();
+            }
+            System.out.println("girdiginiz sayilar : " + Arrays.toString(arr));
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i]>max) {
+                    max=arr[i];
+                }
+
+            }
+            System.out.println("girdiginiz sayilarin en buyugu " + max );
+
+
+
+
+    }
+}
